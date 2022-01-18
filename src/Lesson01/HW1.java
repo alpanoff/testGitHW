20f4abbd59949fe178529a317e14a20a1155568c
alpanoff <alexandr.panov@gmail.com>
2022-01-18 15:12:14 +0500

    Домашнее задание. Урок 1.

--- src/Lesson01/HW1.java
+++ src/Lesson01/HW1.java
@@ -1,2 +1,55 @@
-package Lesson01;public class HW1 {
+package Lesson01;
+
+public class HW1 {
+
+
+    public static void main (String[] args)
+    {
+
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+
+
+    public static void printThreeWords () {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign () {
+        int a = 20;
+        int b = -40;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная.");
+        }
+        else {
+            System.out.println("Сумма отрицательная.");
+        }
+
+    }
+    public static void printColor () {
+        int value = -90;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+    public static void compareNumbers () {
+        int a = 30;
+        int b = 20;
+        if (a >= b) {
+            System.out.println("a>=b");
+        }
+        else {
+            System.out.println("a<b");
+        }
+    }
 }
+
