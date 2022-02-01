b91f15dd2a1f5d62aa5ceb446a56de135400dcb3
alpanoff <alexandr.panov@gmail.com>
2022-02-01 23:35:34 +0500

    5

--- src/Lesson05/online/models/Employee.java
+++ src/Lesson05/online/models/Employee.java
@@ -1,21 +1,20 @@
 package Lesson05.online.models;
 
 public class Employee {
-    private String name;//ФИО
-    private String post;//должность
-    private String email;//email
-    private String phone;//телефон
-    private int salary;//зарплата
-    private int age;//возраст
-
-
-    public Employee(String name, String post, String email, String phone, int salary, int age) {
+        String name;//ФИО
+        String post;//должность
+        String email;//email
+        String phone;//телефон
+        int salary;//зарплата
+        int age;//возраст
+}
+    //создаём конструктор
+    public Employee(String name, String post, String email, String phone, int salary, int age){
         this.name = name;
-        this.age = age;
         this.post = post;
         this.email = email;
         this.phone = phone;
         this.salary = salary;
-    }
-}
+        this.age = age;
 
+    }
