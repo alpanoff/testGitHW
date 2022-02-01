64b8fe6b7e9e0cbfb5202bb519d71032b08c362e
alpanoff <alexandr.panov@gmail.com>
2022-02-01 21:19:02 +0500

    05

--- src/Lesson05/online/models/Employee.java
+++ src/Lesson05/online/models/Employee.java
@@ -1,2 +1,21 @@
-package Lesson05.online.models;public class Employee {
+package Lesson05.online.models;
+
+public class Employee {
+    private String name;//ФИО
+    private String post;//должность
+    private String email;//email
+    private String phone;//телефон
+    private int salary;//зарплата
+    private int age;//возраст
+
+
+    public Employee(String name, String post, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.age = age;
+        this.post = post;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+    }
 }
+
--- src/Lesson05/online/models/HW5.java
+++ src/Lesson05/online/models/HW5.java
@@ -1,2 +1,33 @@
-package Lesson05.online.models;public class HW5 {
+package Lesson05.online.models;
+
+public class HW5 {
+    public static void main(String[] args) {
+//создание объекта person и заполнение полей класса Employee
+        Employee person = new Employee();
+        person.name = "Ivanov Ivan Ivanovich";
+        person.post = "Ingeneer";
+        person.email = "ivanov@mail.com";
+        person.phone = "+79998887755";
+        person.salary = 30000;
+        person.age = 45;
+
+
+
+
+
+    }
+}
+// Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+class Employee {
+    String name;//ФИО
+    String post;//должность
+    String email;//email
+    String phone;//телефон
+    int salary;//зарплата
+    int age;//возраст
+    void infoPerson(){
+        System.out.println("Информация о сотруднике: " + name + post + email + phone + salary + age);
+    }
+
+
 }
